98f44d25f0dbc62705b0f584f7063738b660c9e6
Davide De Rossi <davide.derossi@itiszuccante.edu.it>
2025-11-06 18:59:35 +0100

    Aggregazione e composizione

--- Aggrgation_Composition/src/Main.java
+++ Aggrgation_Composition/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Motor motor = new Motor(150, 110.0, 4, "Fiat", "Fire");
+        List<Wheel> wheels = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            wheels.add(new Wheel("Pirelli", "Cinturato", "205/55 R16", "estivo"));
+        }
 
+        Car macchina = new Car("Fiat", "Punto", "EV252KN", 5, 3, motor, wheels);
+
+        System.out.println(macchina);
     }
 }
--- Aggrgation_Composition/src/Wheel.java
+++ Aggrgation_Composition/src/Wheel.java
@@ -1,2 +1,19 @@
 public class Wheel {
+    private String brand;
+    private String model;
+    private String pneumatic_type;
+    private String type;
+
+    public Wheel(String brand, String model, String pneumatic_type, String type) {
+        this.brand = brand;
+        this.model = model;
+        this.pneumatic_type = pneumatic_type;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Ruota " + brand + " " + model +
+                " (" + pneumatic_type + ", " + type + ")";
+    }
 }
