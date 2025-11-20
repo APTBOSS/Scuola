68d964c3478f24f34a2246a819b3204e37913e73
Davide De Rossi <davide.derossi@itiszuccante.edu.it>
2025-11-20 18:57:27 +0100

    StackManager

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,18 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
-
-        s.push(10);
-        s.push(20);
-        s.push(30);
-
-        s.print();
-
-        System.out.println("Peek: " + s.peek());
-        System.out.println("Pop: " + s.pop());
-
-        s.print();
-
-        System.out.println("Size: " + s.size());
+        StackManager<Integer> integer = new StackManager<>(Integer.class, 3);
+        integer.start();
+        StackManager<String> string = new StackManager<>(String.class, 10);
+        string.start();
     }
 }
